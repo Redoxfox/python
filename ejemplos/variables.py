a12add42cd8473b3081bd19eb4a904434a0cf13b
CarlosTorres <cmtrfenix@gmail.com>
2017-12-25 22:05:16 -0500

    Ejemplos variables

---
+++ ejemplos/variables.py
@@ -0,0 +1,40 @@
+# Esto es un comentario de una sola línea
+# Asignacion de variable corta
+mi_variable = 15
+"""Y este es un comentario
+de varias líneas"""
+# Este comentario es de una línea también
+# Cadena de texto (string):
+mi_cadena = "Hola Mundo!"
+# Cadena de texto linea larga(string):
+mi_cadena_multilinea = """
+Esta es una cadena
+de varias lineas
+"""
+# Imprimir variables
+print (mi_variable)
+#  Número entero:
+edad = 35
+# Número entero octal:
+edad = 0o43
+# Número entero hexadecimal:
+edad = 0x23
+# Número real:
+precio = 7435.28
+# Booleano (verdadero / Falso):
+verdadero = True
+falso = False
+""" + 	Suma 	a = 10 + 5 	a es 15
+- 	Resta 	a = 12 - 7 	a es 5
+- 	Negación 	a = -5 	a es -5
+* 	Multiplicación 	a = 7 * 5 	a es 35
+** 	Exponente 	a = 2 ** 3 	a es 8
+/ 	División 	a = 12.5 / 2 	a es 6.25
+// 	División entera 	a = 12.5 / 2 	a es 6.0
+% 	Módulo 	a = 27 % 4 	a es 3 """
+monto_bruto = 175
+tasa_interes = 12
+monto_interes = monto_bruto * tasa_interes / 100
+tasa_bonificacion = 5
+importe_bonificacion = monto_bruto * tasa_bonificacion / 100
+monto_neto = (monto_bruto - importe_bonificacion) + monto_interes
